bd639bd4b4c2ddab9d3530bad4fb2b5cb2e62bb1
ergl <ergl@users.noreply.github.com>
2016-11-29 16:24:39 +0100

    Simplify configProxy

--- script/configProxy.sh
+++ script/configProxy.sh
@@ -6,22 +6,14 @@ mkdir logs
 AllNodes=`cat /root/basho_bench1/basho_bench/script/allnodesfull`
 echo All nodes "$AllNodes"
 
-Command00="sed -i '/export http_proxy/d' ~/.bashrc"
-Command01="sed -i '/export https_proxy/d' ~/.bashrc"
-Command02="sed -i '10i export https_proxy=https://proxy.sophia.grid5000.fr:3128/' ~/.bashrc"
-Command03="sed -i '10i export http_proxy=http://proxy.sophia.grid5000.fr:3128/' ~/.bashrc"
+# TODO: Why sophia?
+command="\
+  sed -i.bak 's|^export http_proxy.*|export http_proxy=http://proxy.sophia.grid5000.fr:3128|' ~/.bashrc && \
+  sed -i.bak 's|^export https_proxy.*|export https_proxy=https://proxy.sophia.grid5000.fr:3128|' ~/.bashrc
+"
 
 echo Running proxy config
 echo
 
-echo Performing: ./script/parallel_command.sh "$AllNodes" "$Command00"
-./script/parallel_command.sh "$AllNodes" "$Command00" >> logs/config_proxy
-
-echo Performing: ./script/parallel_command.sh "$AllNodes" "$Command01"
-./script/parallel_command.sh "$AllNodes" "$Command01" >> logs/config_proxy
-
-echo Performing: ./script/parallel_command.sh "$AllNodes" "$Command02"
-./script/parallel_command.sh "$AllNodes" "$Command02" >> logs/config_proxy
-
-echo Performing: ./script/parallel_command.sh "$AllNodes" "$Command03"
-./script/parallel_command.sh "$AllNodes" "$Command03" >> logs/config_proxy
+echo Performing: ./script/parallel_command.sh "$AllNodes" "${command}"
+./script/parallel_command.sh "$AllNodes" "${command}" >> logs/config_proxy
