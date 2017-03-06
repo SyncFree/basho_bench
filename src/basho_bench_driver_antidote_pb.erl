85b7f66e3bc5b1a7c222d4d9322abf8b3e047717
Alejandro Z. Tomsic <aletomsic@gmail.com>
2017-03-06 16:09:56 +0100

    added multi-round reads

--- src/basho_bench_driver_antidote_pb.erl
+++ src/basho_bench_driver_antidote_pb.erl
@@ -27,7 +27,7 @@
 -include("basho_bench.hrl").
 -define(BUCKET, <<"antidote_bench_bucket">>).
 
--define(TIMEOUT, 20000).
+-define(TIMEOUT, 100000).
 -record(state, {worker_id,
                 time,
                 type_dict,
