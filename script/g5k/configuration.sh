0d22afb23c527c1c6bbe7754d27e0b5405ed6bd0
Alejandro Z. Tomsic <aletomsic@gmail.com>
2017-03-21 17:39:41 +0100

    added option to change txn_protocol

--- script/g5k/configuration.sh
+++ script/g5k/configuration.sh
@@ -8,7 +8,7 @@ PBKFILE=~/.ssh/id_rsa.pub
 K3_IMAGE=/home/bderegil/public/antidote-images/latest/antidote.env
 
 # JUST RUN, NO CONFIG
-JUST_RUN=false
+JUST_RUN=true
 
 # Reserve sites and nodes through oargridsub
 RESERVE_SITES=false
