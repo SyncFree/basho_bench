47fcd6f94be0c12273d38b7c154cf6dd81b37702
Alejandro Z. Tomsic <aletomsic@gmail.com>
2017-03-31 13:02:49 +0200

    transfer ip

--- script/g5k/main.sh
+++ script/g5k/main.sh
@@ -349,9 +349,10 @@ transferIPs () {
   local antidote_ips_file_name=$(basename "${antidote_ips_file}")
 
   local bench_dc_nodes=( $(< "${bench_node_file}") )
-  echo "copying dc files to nodes!!!"
+  echo "[DISTRIBUTE NODES FILES ] copying dc files to nodes!!!"
   for node in "${bench_dc_nodes[@]}"; do
-    scp -i ${EXPERIMENT_PRIVATE_KEY} "~/basho_bench/script/g5k/.dc_nodes*" root@${node}:/root/
+    command="scp -i ${EXPERIMENT_PRIVATE_KEY} ~/basho_bench/script/g5k/.dc_nodes* root@${node}:/root/"
+    $command
   done
 }
 
