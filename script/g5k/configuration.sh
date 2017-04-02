ab67fa3a8c9ec54e4d563d849840890005d41bc7
Alejandro Z. Tomsic <aletomsic@gmail.com>
2017-04-02 12:33:03 +0200

    fixed bucket change

--- script/g5k/configuration.sh
+++ script/g5k/configuration.sh
@@ -53,7 +53,7 @@ ANTIDOTE_URL="https://github.com/SyncFree/antidote.git"
 # git branch of antidote to run the experiment on
 ANTIDOTE_BRANCH="strict_stable"
 
-ANTIDOTE_PROTOCOLS=( "physics" "clocksi" )
+ANTIDOTE_PROTOCOLS=( "physics" "clocksi" "ec" )
 
 # git repository of the basho_bench code (useful to test forks)
 BENCH_URL="https://github.com/SyncFree/basho_bench.git"
