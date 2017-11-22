9bc4d270935a8c082c718b122eceeafbf52f46dd
aletomsic <aletomsic@gmail.com>
2017-11-22 12:50:17 +0100

    config

--- script/g5k/configuration.sh
+++ script/g5k/configuration.sh
@@ -56,7 +56,8 @@ ANTIDOTE_URL="https://github.com/SyncFree/antidote.git"
 ANTIDOTE_BRANCH="simplify_physics_metadata"
 
 #possible protocols: cure, av, oc, ec, gr
-ANTIDOTE_PROTOCOLS=( "cure" "av" "oc" "gr" "ec" )
+#ANTIDOTE_PROTOCOLS=( "cure" "av" "oc" "gr" "ec" )
+ANTIDOTE_PROTOCOLS=( "cure" )
 
 # git repository of the basho_bench code (useful to test forks)
 BENCH_URL="https://github.com/SyncFree/basho_bench.git"
@@ -80,7 +81,7 @@ KEY_SPACES=( 100000 )
 ROUND_NUMBER=( 1 )
 READ_NUMBER=( 100 )
 UPDATE_NUMBER=( 50 )
-BENCH_THREAD_NUMBER=( 25 )
+BENCH_THREAD_NUMBER=( 5 10 15 20 25 )
 
 #6 exponential
 #KEY_SPACES=( 10000000 1000000 )
