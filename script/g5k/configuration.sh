94336104d2e0187be394db07fe8b14edadb06e2c
aletomsic <aletomsic@gmail.com>
2017-11-23 16:44:05 +0100

    changed config

--- script/g5k/configuration.sh
+++ script/g5k/configuration.sh
@@ -19,7 +19,7 @@ SITES=( "nantes" )
 GRID_JOB_ID=60016
 
 # Boot the machines and load the os image.
-DEPLOY_IMAGE=true
+DEPLOY_IMAGE=false
 
 # Provision the nodes with Antidote / basho_bench
 DOWNLOAD_ANTIDOTE=true
