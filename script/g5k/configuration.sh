a279442d3d2943a760c4a07575ed729bffb875e7
aletomsic <aletomsic@gmail.com>
2017-11-24 12:44:55 +0100

    config

--- script/g5k/configuration.sh
+++ script/g5k/configuration.sh
@@ -22,9 +22,9 @@ GRID_JOB_ID=60020
 DEPLOY_IMAGE=false
 
 # Provision the nodes with Antidote / basho_bench
-DOWNLOAD_ANTIDOTE=true
+DOWNLOAD_ANTIDOTE=false
 
-DOWNLOAD_BENCH=true
+DOWNLOAD_BENCH=false
 
 # Download and compile antidote and basho bench from scratch
 CLEAN_ANTIDOTE=true
