9aa32a8236e2f3bcdae2576134b8addccba2cbd5
aletomsic <aletomsic@gmail.com>
2017-11-24 13:17:43 +0100

    changed config

--- script/g5k/configuration.sh
+++ script/g5k/configuration.sh
@@ -38,14 +38,14 @@ CONNECT_CLUSTERS_AND_DCS=true
 # Number of "data centers" per g5k site
 # For example, saying DCS_PER_SITE=2 and ANTIDOTE_NODES=1
 # will create 2 antidote nodes in total, one on each data center
-DCS_PER_SITE=1
+DCS_PER_SITE=2
 
 # Run a bench_node per antidote node (and dismiss the BENCH_NODES param)
 BENCH_THE_LOCAL_NODE=true
 # Number of nodes running Antidote PER DC!!!!!!
-ANTIDOTE_NODES=4 #PER DC!
+ANTIDOTE_NODES=2 #PER DC!
 # Number of nodes running Basho Bench per DC
-BENCH_NODES=4 #PER DC!
+BENCH_NODES=2 #PER DC!
 # Number of instances of basho_bench to run per node
 BENCH_INSTANCES=2 #PER BENCH_NODE!
 
