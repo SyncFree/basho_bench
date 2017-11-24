7022af6277e4b3cff965061bb0b842cc79bd128a
aletomsic <aletomsic@gmail.com>
2017-11-24 23:26:24 +0100

    added supermain

--- script/g5k/super_main.sh
+++ script/g5k/super_main.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
-
+i=5
+while [ $i -lt 4 ]
+do
 set -eo pipefail
 ###### first run single round
-mv -f ~/basho_bench/script/g5k/configuration-single-dc-single-round-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
+cp -f ~/basho_bench/script/g5k/configuration-single-dc-single-round-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
 ~/basho_bench/script/g5k/main.sh
-mv -f ~/basho_bench/script/g5k/configuration-2-dc-single-round-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
+cp -f ~/basho_bench/script/g5k/configuration-2-dc-single-round-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
 ~/basho_bench/script/g5k/main.sh
-mv -f ~/basho_bench/script/g5k/configuration-3-dc-single-round-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
+cp -f ~/basho_bench/script/g5k/configuration-3-dc-single-round-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
 ~/basho_bench/script/g5k/main.sh
 ###### facebook
-mv -f ~/basho_bench/script/g5k/configuration-single-dc-facebook-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
+cp -f ~/basho_bench/script/g5k/configuration-single-dc-facebook-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
 ~/basho_bench/script/g5k/main.sh
-mv -f ~/basho_bench/script/g5k/configuration-2-dc-facebook-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
+cp -f ~/basho_bench/script/g5k/configuration-2-dc-facebook-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
 ~/basho_bench/script/g5k/main.sh
-mv -f ~/basho_bench/script/g5k/configuration-3-dc-facebook-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
+cp -f ~/basho_bench/script/g5k/configuration-3-dc-facebook-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
 ~/basho_bench/script/g5k/main.sh
 ###### 5round
-mv -f ~/basho_bench/script/g5k/configuration-single-dc-5round-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
+cp -f ~/basho_bench/script/g5k/configuration-single-dc-5round-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
 ~/basho_bench/script/g5k/main.sh
-mv -f ~/basho_bench/script/g5k/configuration-2-dc-5round-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
+cp -f ~/basho_bench/script/g5k/configuration-2-dc-5round-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
 ~/basho_bench/script/g5k/main.sh
-mv -f ~/basho_bench/script/g5k/configuration-3-dc-5round-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
+cp -f ~/basho_bench/script/g5k/configuration-3-dc-5round-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
 ~/basho_bench/script/g5k/main.sh
+done
