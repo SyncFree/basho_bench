268872cd614ae0ce0e932798266a9acd01aa20e0
aletomsic <aletomsic@gmail.com>
2017-11-27 16:49:14 +0100

    change config

--- script/g5k/configuration-single-dc-single-round-local-100r.sh
+++ script/g5k/configuration-single-dc-single-round-local-100r.sh
@@ -58,7 +58,7 @@ ANTIDOTE_URL="https://github.com/SyncFree/antidote.git"
 ANTIDOTE_BRANCH="simplify_physics_metadata"
 
 #possible protocols: cure, av, oc, ec, gr
-ANTIDOTE_PROTOCOLS=( "cure" "av" "oc" "ec" )
+ANTIDOTE_PROTOCOLS=( "av" "oc" "ec" )
 
 # git repository of the basho_bench code (useful to test forks)
 BENCH_URL="https://github.com/SyncFree/basho_bench.git"
