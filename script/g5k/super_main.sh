b986749db3479984a5bb71003b0fce281ed26867
aletomsic <aletomsic@gmail.com>
2017-11-28 18:19:46 +0100

    change config

--- script/g5k/super_main.sh
+++ script/g5k/super_main.sh
@@ -11,15 +11,15 @@ set -eo pipefail
 #cp -f ~/basho_bench/script/g5k/configuration-3-dc-single-round-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
 #~/basho_bench/script/g5k/main.sh
 ###### facebook
-cp -f ~/basho_bench/script/g5k/configuration-single-dc-facebook-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
-~/basho_bench/script/g5k/main.sh
-#cp -f ~/basho_bench/script/g5k/configuration-2-dc-facebook-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
+#cp -f ~/basho_bench/script/g5k/configuration-single-dc-facebook-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
 #~/basho_bench/script/g5k/main.sh
+cp -f ~/basho_bench/script/g5k/configuration-2-dc-facebook-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
+~/basho_bench/script/g5k/main.sh
 #cp -f ~/basho_bench/script/g5k/configuration-3-dc-facebook-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
 #~/basho_bench/script/g5k/main.sh
 ###### 5round
-cp -f ~/basho_bench/script/g5k/configuration-single-dc-5round-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
-~/basho_bench/script/g5k/main.sh
+#cp -f ~/basho_bench/script/g5k/configuration-single-dc-5round-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
+#~/basho_bench/script/g5k/main.sh
 #cp -f ~/basho_bench/script/g5k/configuration-2-dc-5round-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
 #~/basho_bench/script/g5k/main.sh
 #cp -f ~/basho_bench/script/g5k/configuration-3-dc-5round-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
