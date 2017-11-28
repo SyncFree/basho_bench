2156b6f2786cf03e229adfd02790c80a97a66f7f
aletomsic <aletomsic@gmail.com>
2017-11-28 09:11:25 +0100

    change config

--- script/g5k/configuration-single-dc-facebook-local-100r.sh
+++ script/g5k/configuration-single-dc-facebook-local-100r.sh
@@ -16,10 +16,10 @@ RESERVE_SITES=false
 SITES=( "rennes" )
 
 # Comment or remove this line when RESERVE_SITES=true, it will be added automatically.
-GRID_JOB_ID=60028
+GRID_JOB_ID=60031
 
 # Boot the machines and load the os image.
-DEPLOY_IMAGE=false
+DEPLOY_IMAGE=true
 
 # Provision the nodes with Antidote / basho_bench
 DOWNLOAD_ANTIDOTE=true
@@ -58,7 +58,7 @@ ANTIDOTE_URL="https://github.com/SyncFree/antidote.git"
 ANTIDOTE_BRANCH="simplify_physics_metadata"
 
 #possible protocols: cure, av, oc, ec, gr
-ANTIDOTE_PROTOCOLS=( "av" "oc" "ec" )
+ANTIDOTE_PROTOCOLS=( "cure" "av" "oc" "ec" )
 
 # git repository of the basho_bench code (useful to test forks)
 BENCH_URL="https://github.com/SyncFree/basho_bench.git"
