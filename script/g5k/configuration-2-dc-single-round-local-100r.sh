a2594c43691194771e6ae9dbe1a1df3fe4400112
aletomsic <aletomsic@gmail.com>
2017-11-30 09:05:47 +0100

    changed config

--- script/g5k/configuration-2-dc-single-round-local-100r.sh
+++ script/g5k/configuration-2-dc-single-round-local-100r.sh
@@ -16,7 +16,7 @@ RESERVE_SITES=false
 SITES=( "rennes" )
 
 # Comment or remove this line when RESERVE_SITES=true, it will be added automatically.
-GRID_JOB_ID=60002
+GRID_JOB_ID=60041
 
 # Boot the machines and load the os image.
 DEPLOY_IMAGE=true
@@ -43,9 +43,9 @@ DCS_PER_SITE=2
 # Run a bench_node per antidote node (and dismiss the BENCH_NODES param)
 BENCH_THE_LOCAL_NODE=true
 # Number of nodes running Antidote PER DC!!!!!!
-ANTIDOTE_NODES=8 #PER DC!
+ANTIDOTE_NODES=4 #PER DC!
 # Number of nodes running Basho Bench per DC
-BENCH_NODES=8 #PER DC!
+BENCH_NODES=4 #PER DC!
 # Number of instances of basho_bench to run per node
 BENCH_INSTANCES=2 #PER BENCH_NODE!
 
@@ -76,4 +76,4 @@ KEY_SPACES=( 1000000 100000 10000 )
 ROUND_NUMBER=( 1 )
 READ_NUMBER=( 100 100 100 100 100 100 )
 UPDATE_NUMBER=( 2 10 25 50 75 100 )
-BENCH_THREAD_NUMBER=( 5 10 15 25 25 40 50)
+BENCH_THREAD_NUMBER=( 5 10 15 20 25 40 50)
