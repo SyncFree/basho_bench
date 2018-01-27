439aad4fa8305caeedaebde2b37b1e6b8d3bfb39
aletomsic <aletomsic@gmail.com>
2018-01-27 17:38:50 +0100

    changed configs  and path to antidote

--- script/g5k/main.sh
+++ script/g5k/main.sh
@@ -200,6 +200,7 @@ provisionAntidote () {
   echo -e "\t[PROVISION_ANTIDOTE_NODES]: Starting... (This may take a while)"
 
   local command="\
+    rm -rf antidote && \
     mkdir -p /tmp/antidote && \
     ln -s /tmp/antidote ~/antidote && \
     rm -rf antidote/* && \
