2a9978823e47cab4859719370add916e2fffba68
aletomsic <aletomsic@gmail.com>
2018-01-30 12:19:58 +0100

    run rennes

--- script/g5k/configuration-2-dc-single-round-local-100r.sh
+++ script/g5k/configuration-2-dc-single-round-local-100r.sh
@@ -58,7 +58,7 @@ ANTIDOTE_URL="https://github.com/SyncFree/antidote.git"
 ANTIDOTE_BRANCH="17-dec"
 
 #possible protocols: cure, av, oc, ec, gr
-ANTIDOTE_PROTOCOLS=( "ec" )
+ANTIDOTE_PROTOCOLS=( "ec" "oc" "av" "cure" )
 
 # git repository of the basho_bench code (useful to test forks)
 BENCH_URL="https://github.com/SyncFree/basho_bench.git"
