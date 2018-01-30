50eb8babad07eefa1ba4f4e7b921d51148a54cd3
aletomsic <aletomsic@gmail.com>
2018-01-30 21:34:10 +0100

    run rennes

--- script/g5k/rennes_super_main.sh
+++ script/g5k/rennes_super_main.sh
@@ -5,15 +5,6 @@
 set -eo pipefail
 
 
-###### facebook
-#cp -f ~/basho_bench/script/g5k/configuration-single-dc-facebook-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
-#~/basho_bench/script/g5k/main.sh
-cp -f ~/basho_bench/script/g5k/configuration-2-dc-facebook-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
-~/basho_bench/script/g5k/main.sh
-#cp -f ~/basho_bench/script/g5k/configuration-3-dc-facebook-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
-#~/basho_bench/script/g5k/main.sh
-
-
 ###### first run single round
 cp -f ~/basho_bench/script/g5k/configuration-single-dc-single-round-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
 ~/basho_bench/script/g5k/main.sh
@@ -24,7 +15,13 @@ cp -f ~/basho_bench/script/g5k/configuration-single-dc-single-round-local-2.sh ~
 #cp -f ~/basho_bench/script/g5k/configuration-3-dc-single-round-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
 #~/basho_bench/script/g5k/main.sh
 
-
+###### facebook
+#cp -f ~/basho_bench/script/g5k/configuration-single-dc-facebook-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
+#~/basho_bench/script/g5k/main.sh
+cp -f ~/basho_bench/script/g5k/configuration-2-dc-facebook-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
+~/basho_bench/script/g5k/main.sh
+#cp -f ~/basho_bench/script/g5k/configuration-3-dc-facebook-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
+#~/basho_bench/script/g5k/main.sh
 
 ###### 5round
 #cp -f ~/basho_bench/script/g5k/configuration-single-dc-5round-local-100r.sh ~/basho_bench/script/g5k/configuration.sh
